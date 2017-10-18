428dfc97cc4974207faefcf6e8987c9343a51be1
Srikanth R <srikanthalways@gmail.com>
2017-10-18 15:17:39 +0530

    SampleCommit

---
+++ SampleWebForAzure/Controllers/TrialController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebForAzure.Controllers
+{
+    public class TrialController : Controller
+    {
+        // GET: Trial
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TrialAction()
+        {
+            //JsonResult jsr = new JsonResult();
+            //jsr.Data = "Hi From Trial Azure";
+            //return jsr;
+            return Json("Hi From Trial Azure", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
